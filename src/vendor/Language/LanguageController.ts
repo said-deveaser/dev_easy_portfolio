ddcc1e74b884c933cf3c18b09360754c04546a5b
Said Tashmanov <59595711+said-deveaser@users.noreply.github.com>
2021-10-01 14:10:10 +0600

    Update LanguageController.ts

--- src/vendor/Language/LanguageController.ts
+++ src/vendor/Language/LanguageController.ts
@@ -18,7 +18,7 @@ class LanguageController<LP> {
         this.languagePointerWrapper = languagePointerWrapper
     }
 
-    get(key: keyof LP) {
+    get<T extends keyof LP>(key: T):LP[T] {
         const defaultLang = process.env.DEFAULT_LANG as LanguageEnum
         const lang:LanguageEnum = Object.values(LanguageEnum).includes(defaultLang) ?  defaultLang : LanguageEnum.RU
         return this.languagePointerWrapper[lang][key]
